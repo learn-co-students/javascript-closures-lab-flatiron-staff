1624664a61f1d6ffcf16b4e58dcefcddb9b504d1
Haoshuang Wang <haoshuang@flatironschool.com>
2017-10-02 01:35:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+  function addBump() {
+    return counter += 1
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType) {
+  function deadlyDevice(deviceType) {
+    return {animalType: `${animalType}`, deadlyDevice: `${deviceType}`}
+  }
+  return deadlyDevice
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
